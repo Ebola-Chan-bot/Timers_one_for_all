73af5bae9f14ad727bcbca65267ee686a2d236ab
埃博拉酱 <vhtmfu@outlook.com>
2024-04-10 22:04:58 +0800

    实现StartTiming

--- src/Timers_one_for_all_2.cpp
+++ src/Timers_one_for_all_2.cpp
@@ -8,6 +8,40 @@ const TimerClass *Timers_one_for_all::AllocateIdleTimer()
 			return HardwareTimers[T];
 	return nullptr;
 }
+template <uint8_t... Values>
+using U8Sequence = std::integer_sequence<uint8_t, Values...>;
+template <uint8_t First, typename T>
+struct U8SequencePrepend;
+template <uint8_t First, uint8_t... Rest>
+struct U8SequencePrepend<First, U8Sequence<Rest...>>
+{
+	using type = U8Sequence<First, Rest...>;
+};
+template <typename T>
+struct U8SequenceDiff;
+template <uint8_t First, uint8_t Second, uint8_t... Rest>
+struct U8SequenceDiff<U8Sequence<First, Second, Rest...>>
+{
+	using type = U8SequencePrepend<Second - First, U8SequenceDiff<U8Sequence<Second, Rest...>>::type>::type;
+};
+template <uint8_t First>
+struct U8SequenceDiff<U8Sequence<First>>
+{
+	using type = U8Sequence<>;
+};
+template <typename T>
+struct PrescalerDiff;
+template <uint8_t... Diff>
+struct PrescalerDiff<U8Sequence<Diff...>>
+{
+	static constexpr uint8_t AdvanceFactor[] = {1 << Diff...};
+};
+template <uint8_t... BitShift>
+struct PrescalerType : public PrescalerDiff<U8SequenceDiff<U8Sequence<BitShift...>>::type>
+{
+	static constexpr uint8_t BitShifts[] = {BitShift...};
+	static constexpr uint8_t NumPrescalers = sizeof...(BitShift);
+};
 #ifdef ARDUINO_ARCH_AVR
 #ifdef TOFA_TIMER0
 #warning 使用计时器0可能导致内置 micros millis delay 等时间相关函数工作异常。如果不需要使用这些内置函数，可以忽略此警告；否则考虑取消宏定义TOFA_TIMER0
@@ -49,6 +83,8 @@ TimerIsr(4);
 #ifdef TOFA_TIMER5
 TimerIsr(5);
 #endif
+using Prescaler01 = PrescalerType<0, 3, 6, 8, 10>;
+using Prescaler2 = PrescalerType<0, 3, 5, 6, 7, 8, 10>;
 void TimerClass::Pause() const
 {
 	const uint8_t Clock = TCCRB & 0b111;
@@ -62,5 +98,60 @@ void TimerClass::Continue() const
 {
 	if (!(TCCRB & 0b111))
 		TCCRB |= State.Clock;
+}
+template <typename PrescalerType>
+void StartTiming(TimerClass *Timer)
+{
+	Timer->TIFR = -1;
+	interrupts();
+	Timer->State.OverflowCount = 0;
+	Timer->State.OverflowTarget = PrescalerType::AdvanceFactor[0];
+	Timer->State.OVFCOMPA = [Timer]()
+	{
+		_TimerState &State = Timer->State;
+		uint32_t &OverflowCount = State.OverflowCount;
+		if (++OverflowCount == State.OverflowTarget)
+		{
+			const uint8_t TCCRB = Timer->TCCRB++;
+			OverflowCount = 1;
+			if ((TCCRB < PrescalerType::NumPrescalers - 1))
+				State.OverflowTarget = PrescalerType::AdvanceFactor[TCCRB];
+			else
+				State.OVFCOMPA = [&OverflowCount]()
+				{ OverflowCount++; };
+		}
+	};
+}
+void TimerClass0::StartTiming() const
+{
+	noInterrupts();
+	TCNT0 = 0;
+	TCCR0B = 1;
+	TCCR0A = 0;
+	OCR0A = 0;
+	TIMSK0 = 1 << OCIE0A;
+	::StartTiming<Prescaler01>((TimerClass *)this);
+}
+void TimerClass1::StartTiming() const
+{
+	noInterrupts();
+	TCNT = 0;
+	TCCRB = 1;
+	TCCRA = 0;
+	TIMSK = 1 << TOIE1;
+	::StartTiming<Prescaler01>((TimerClass *)this);
+}
+void TimerClass2::StartTiming() const
+{
+	noInterrupts();
+	TCNT2 = 0;
+	TCCR2B = 1;
+	TCCR2A = 0;
+	TIMSK2 = 1 << TOIE1;
+	::StartTiming<Prescaler2>((TimerClass *)this);
+}
+Tick TimerClass0::GetTiming()const
+{
+	(State.OverflowCount<<8)+TCNT0
 }
 #endif
--- src/Timers_one_for_all_2.hpp
+++ src/Timers_one_for_all_2.hpp
@@ -57,61 +57,54 @@ namespace Timers_one_for_all
 		uint8_t Clock;
 		std::function<void()> OVFCOMPA;
 		std::function<void()> COMPB;
-		std::function<void()> DoneCallback;
 		uint32_t OverflowCount;
-		uint32_t OverflowTargetA;
-		uint32_t OverflowTargetB;
+		uint32_t OverflowTarget;
 		uint64_t RepeatLeft;
 	};
 	extern _TimerState _TimerStates[NumTimers];
 	using Tick = std::chrono::duration<uint64_t, std::ratio<1, F_CPU>>;
 	constexpr uint64_t InfiniteRepeat = -1;
-	enum class Exception
-	{
-		Successful_operation,
-		Timer_already_paused,
-		Timer_not_paused,
-	};
 	struct TimerClass
 	{
-		//检查计时器是否忙碌。暂停的计时器也属于忙碌。
+		_TimerState &State;
+		uint8_t &TCCRA;
+		uint8_t &TCCRB;
+		uint8_t &TIMSK;
+		uint8_t &TIFR;
+		// 检查计时器是否忙碌。暂停的计时器也属于忙碌。
 		bool Busy() const { return TIMSK; }
-		//暂停计时器。暂停一个已暂停的计时器将返回Timer_already_paused
-		Exception Pause() const;
-		//继续计时器。继续一个未暂停的计时器将返回Timer_not_paused
-		Exception Continue() const;
-		//终止计时器并设为空闲。一旦终止，任务将不能恢复。
+		// 暂停计时器。暂停一个已暂停的计时器将不做任何事
+		void Pause() const;
+		// 继续计时器。继续一个未暂停的计时器将不做任何事
+		void Continue() const;
+		// 终止计时器并设为空闲。一旦终止，任务将不能恢复。
 		void Stop() const { TIMSK = 0; }
-		//开始计时任务。稍后可用GetTiming获取已记录的时间。
+		// 开始计时任务。稍后可用GetTiming获取已记录的时间。
 		virtual void StartTiming() const = 0;
-		virtual Tick GetTiming() const = 0;
-		//获取已记录的时间，模板参数指定要返回的std::chrono::duration时间格式。
+		// 获取已记录的时间，模板参数指定要返回的std::chrono::duration时间格式。
 		template <typename T>
 		T GetTiming() const { return std::chrono::duration_cast<T>(GetTiming()); }
 		virtual void Delay(Tick) const = 0;
-		//阻塞指定的时长。在主线程中调用将仅阻塞主线程，可以被其它线程中断。在中断线程中调用将阻塞所有线程，无法被中断，可能导致其它依赖中断的任务出现未定义行为。
+		// 阻塞Duration时长。在主线程中调用将仅阻塞主线程，可以被其它线程中断。在中断线程中调用将阻塞所有线程，无法被中断，可能导致其它依赖中断的任务出现未定义行为。
 		template <typename T>
 		void Delay(T Duration) const { Delay(std::chrono::duration_cast<Tick>(Duration)); }
-		virtual void DoAfter(Tick After, std::function<void()> Do) const = 0;
-		//在指定时间后执行指定任务。不同于Delay，此方法不会阻塞当前线程，而是在指定时间后发起新的中断线程来执行任务
+		// 在After时间后执行Do。不同于Delay，此方法不会阻塞当前线程，而是在指定时间后发起新的中断线程来执行任务
 		template <typename T>
 		void DoAfter(T After, std::function<void()> Do) const { DoAfter(std::chrono::duration_cast<Tick>(After), Do); }
-		virtual void RepeatEvery(Tick Every, std::function<void()> Do, uint64_t RepeatTimes, std::function<void()> DoneCallback) const = 0;
-		//每隔指定时间就重复执行任务，第一次执行也在指定时间之后。可选额外指定重复次数（默认无限重复）和所有重复结束后立即执行的回调。
+		// 每隔指定时间就重复执行任务，第一次执行也在指定时间之后。可选额外指定重复次数（默认无限重复）和所有重复结束后立即执行的回调。
 		template <typename T>
 		void RepeatEvery(
 			T Every, std::function<void()> Do, uint64_t RepeatTimes = InfiniteRepeat, std::function<void()> DoneCallback = []() {}) const
 		{
 			RepeatEvery(std::chrono::duration_cast<Tick>(Every), Do, RepeatTimes, DoneCallback);
 		}
-		//每隔指定时间就重复执行任务，第一次执行也在指定时间之后，但仅持续
+		// 每隔指定时间就重复执行任务，第一次执行也在指定时间之后，但仅持续
 		template <typename T>
 		void RepeatEvery(
 			T Every, std::function<void()> Do, T RepeatDuration, std::function<void()> DoneCallback = []() {}) const
 		{
-			RepeatEvery(std::chrono::duration_cast<Tick>(Every), Do, RepeatDuration / Every, DoneCallback);
+			RepeatEvery(std::chrono::duration_cast<Tick>(Every), Do, RepeatDuration, DoneCallback);
 		}
-		virtual void DoubleRepeat(Tick AfterA, std::function<void()> DoA, Tick AfterB, std::function<void()> DoB, uint64_t NumHalfPeriods, std::function<void()> DoneCallback) const = 0;
 		template <typename T>
 		void DoubleRepeat(
 			T AfterA, std::function<void()> DoA, T AfterB, std::function<void()> DoB, uint64_t NumHalfPeriods = InfiniteRepeat, std::function<void()> DoneCallback = []() {}) const
@@ -122,11 +115,8 @@ namespace Timers_one_for_all
 		void DoubleRepeat(
 			T AfterA, std::function<void()> DoA, T AfterB, std::function<void()> DoB, T RepeatDuration, std::function<void()> DoneCallback = []() {}) const
 		{
-			const T FullPeriod = AfterA + AfterB;
-			DoubleRepeat(std::chrono::duration_cast<Tick>(AfterA), DoA, std::chrono::duration_cast<Tick>(AfterB), DoB, uint64_t(RepeatDuration / FullPeriod) * 2 + uint64_t(RepeatDuration % FullPeriod / AfterA), DoneCallback)
+			DoubleRepeat(std::chrono::duration_cast<Tick>(AfterA), DoA, std::chrono::duration_cast<Tick>(AfterB), DoB, RepeatDuration, DoneCallback)
 		}
-		virtual void RandomRepeat(std::function<Tick()> RandomGenerator, std::function<void()> Do, uint64_t RepeatTimes, std::function<void()> DoneCallback) const = 0;
-		virtual void RandomRepeat(std::function<Tick()> RandomGenerator, std::function<void()> Do, Tick RepeatDuration, std::function<void()> DoneCallback) const = 0;
 		template <typename T>
 		void RandomRepeat(
 			std::function<T()> RandomGenerator, std::function<void()> Do, uint64_t RepeatTimes = InfiniteRepeat, std::function<void()> DoneCallback = []() {}) const
@@ -145,12 +135,15 @@ namespace Timers_one_for_all
 		}
 
 	protected:
-		_TimerState &State;
-		uint8_t &TCCRA;
-		uint8_t &TCCRB;
-		uint8_t &TIMSK;
-		uint8_t &TIFR;
 		constexpr TimerClass(_TimerState &State, uint8_t &TCCRA, uint8_t &TCCRB, uint8_t &TIMSK, uint8_t &TIFR) : State(State), TCCRA(TCCRA), TCCRB(TCCRB), TIMSK(TIMSK), TIFR(TIFR) {}
+		virtual Tick GetTiming() const = 0;
+		virtual void DoAfter(Tick After, std::function<void()> Do) const = 0;
+		virtual void RepeatEvery(Tick Every, std::function<void()> Do, uint64_t RepeatTimes, std::function<void()> DoneCallback) const = 0;
+		virtual void RepeatEvery(Tick Every, std::function<void()> Do, Tick RepeatDuration, std::function<void()> DoneCallback) const = 0;
+		virtual void DoubleRepeat(Tick AfterA, std::function<void()> DoA, Tick AfterB, std::function<void()> DoB, uint64_t NumHalfPeriods, std::function<void()> DoneCallback) const = 0;
+		virtual void DoubleRepeat(Tick AfterA, std::function<void()> DoA, Tick AfterB, std::function<void()> DoB, Tick RepeatDuration, std::function<void()> DoneCallback) const = 0;
+		virtual void RandomRepeat(std::function<Tick()> RandomGenerator, std::function<void()> Do, uint64_t RepeatTimes, std::function<void()> DoneCallback) const = 0;
+		virtual void RandomRepeat(std::function<Tick()> RandomGenerator, std::function<void()> Do, Tick RepeatDuration, std::function<void()> DoneCallback) const = 0;
 	};
 	template <typename T>
 	struct _TimerBitClass : public TimerClass
@@ -165,12 +158,15 @@ namespace Timers_one_for_all
 	struct TimerClass0 : public _TimerBitClass<uint8_t>
 	{
 		constexpr TimerClass0(_TimerState &State, uint8_t &TCCRA, uint8_t &TCCRB, uint8_t &TIMSK, uint8_t &TIFR, uint8_t &TCNT, uint8_t &OCRA, uint8_t &OCRB) : _TimerBitClass(State, TCCRA, TCCRB, TIMSK, TIFR, TCNT, OCRA, OCRB) {}
+		void StartTiming() const override;
+		Tick GetTiming() const override;
 	};
 	constexpr TimerClass0 HardwareTimer0(_TimerStates[(size_t)TimerEnum::Timer0], (uint8_t &)TCCR0A, (uint8_t &)TCCR0B, (uint8_t &)TIMSK0, (uint8_t &)TIFR0, (uint8_t &)TCNT0, (uint8_t &)OCR0A, (uint8_t &)OCR0B);
 #endif
 	struct TimerClass1 : public _TimerBitClass<uint16_t>
 	{
 		constexpr TimerClass1(_TimerState &State, uint8_t &TCCRA, uint8_t &TCCRB, uint8_t &TIMSK, uint8_t &TIFR, uint16_t &TCNT, uint16_t &OCRA, uint16_t &OCRB) : _TimerBitClass(State, TCCRA, TCCRB, TIMSK, TIFR, TCNT, OCRA, OCRB) {}
+		void StartTiming() const override;
 	};
 #ifdef TOFA_TIMER1
 	constexpr TimerClass1 HardwareTimer1(_TimerStates[(size_t)TimerEnum::Timer1], (uint8_t &)TCCR1A, (uint8_t &)TCCR1B, (uint8_t &)TIMSK1, (uint8_t &)TIFR1, (uint16_t &)TCNT1, (uint16_t &)OCR1A, (uint16_t &)OCR1B);
@@ -179,6 +175,7 @@ namespace Timers_one_for_all
 	struct TimerClass2 : public _TimerBitClass<uint8_t>
 	{
 		constexpr TimerClass2(_TimerState &State, uint8_t &TCCRA, uint8_t &TCCRB, uint8_t &TIMSK, uint8_t &TIFR, uint8_t &TCNT, uint8_t &OCRA, uint8_t &OCRB) : _TimerBitClass(State, TCCRA, TCCRB, TIMSK, TIFR, TCNT, OCRA, OCRB) {}
+		void StartTiming() const override;
 	};
 	constexpr TimerClass2 HardwareTimer2(_TimerStates[(size_t)TimerEnum::Timer2], (uint8_t &)TCCR2A, (uint8_t &)TCCR2B, (uint8_t &)TIMSK2, (uint8_t &)TIFR2, (uint8_t &)TCNT2, (uint8_t &)OCR2A, (uint8_t &)OCR2B);
 #endif
